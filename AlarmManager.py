499489e38f79a26ebd21fbdf7ac14cad2482f4f2
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-08-20 10:40:21 +0900

    edited by hangkuk at 2021.08.20

--- AlarmManager.py
+++ AlarmManager.py
@@ -419,7 +419,7 @@ class AlarmMgr:
 
                     # To do later.....alarm_category
                     alarm_category = 'TEST_5G_CATEGORY'
-                    if __get_alarm_category(alarm_category, probable_cause) != True:
+                    if self.__get_alarm_category(alarm_category, probable_cause) != True:
                         print(f'__get_alarm_category() fail. alarm_category=[{alarm_category}]')
 
                     print(f'__get_alarm_category() success!. alarm_category=[{alarm_category}]')
