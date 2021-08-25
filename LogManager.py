a475709bf840571dc9243e1556717bc0bcf178e2
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-08-25 11:53:32 +0900

    edited by hangkuk at 2021.08.25

--- LogManager.py
+++ LogManager.py
@@ -16,4 +16,52 @@ class LogManager:
             LogManager.__instance = LogManager()
         return LogManager.__instance
 
+    def __init__(self):
+
+        self.logger = logging.getLogger("msglog")
+        self.logger.setLevel(logging.DEBUG)
+
+        currentDate = datetime.datetime.now()
+        current     = currentDate.strftime('%Y-%m-%d')
+
+        self.set_fileHandler(current)
+
+    def set_fileHandler(self, current):
+
+        maxSize         = 10400
+        maxBackupCnt    = 10
+
+        try:
+            home_str = os.environ["HOME"]
+        except:
+            home_str = '/data1/e2e'
+
+        # File
+        dirLoc  = '%s/log/MSG_LOG/SYNC/%s' % (home_str , current)
+        fileLoc = dirLoc + "/pyscript_file.log"
+
+        if not os.path.exists(dirLoc):
+            os.makedirs(dirLoc)
+
+        # 10 MB ( Automation File Control )
+        self.fileHandler = logging.handlers.RotatingFileHandler(filename=fileLoc, maxBytes=maxSize, backupCount=maxBackupCnt, encoding='utf-8')
+
+        #formatter
+        self.formatter = logging.Formatter('[%(asctime)s|%(filename)s:%(lineno)s][%(levelname)s] > %(message)s')
+        self.fileHandler.setFormatter(self.formatter)
+        self.logger.handlers = []
+        self.logger.addHandler(self.fileHandler)
+
+    def updateLogLoc(self):
+
+        currentDate = datetime.datetime.now()
+
+        if  currentDate.strftime('%H:%M:%S') == "00:00:00":
+
+            current     = currentDate.strftime('%Y-%m-%d')
+            LogManager.getInstance().set_fileHandler(current)
+
+    def get_logger(self):
+        # type: () -> object
+        return self.logger
 
