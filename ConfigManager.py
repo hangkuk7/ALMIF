68673c1c5393114fbfa9c07f213f8d82f5bf4a40
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-08-27 15:06:50 +0900

    edited by hangkuk at 2021.08.27

--- ConfigManager.py
+++ ConfigManager.py
@@ -103,13 +103,14 @@ class ConfManager:
                 self.msg_log_level = 5
 
             # [TIME_CONFIG] section
-            self.time_interval = int(self.getLocalConfigData("TIME_CONFIG", "TIME_INTERVAL", 1))
-            if self.time_interval < 1:
-                self.time_interval = 30
-            elif self.time_interval > 600:
-                self.time_interval = 600
-            else:
-                pass
+            # self.time_interval = int(self.getLocalConfigData("TIME_CONFIG", "TIME_INTERVAL", 1))
+            self.time_interval = self.getLocalConfigData("TIME_CONFIG", "TIME_INTERVAL", 1)
+            # if self.time_interval < 1:
+            #     self.time_interval = 30
+            # elif self.time_interval > 600:
+            #     self.time_interval = 600
+            # else:
+            #     pass
 
         except Exception as errmsg:
             logger.critical('Error. Exception in read LocalConfig config : {}'.format(errmsg))
