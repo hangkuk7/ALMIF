f28caf4c7b3eeb540bbf9bb8616063f8026b7fc2
hangkuk7 <74293602+hangkuk7@users.noreply.github.com>
2021-08-27 13:24:47 +0900

    edited by hangkuk at 2021.08.27

--- ScheduleManager.py
+++ ScheduleManager.py
@@ -1,8 +1,19 @@
 from datetime import datetime
 from datetime import timedelta
 
+from LogManager import LogManager
+
+# For log
+logger = LogManager.getInstance().get_logger()
+
 class ScheduleManager:
-    def __init__(self):
+    def __init__(self, interval):
+        if interval < 1:
+            logger.critical(f'[ScheduleManager init] Error. Invalid interval. interval=[{interval}]')
+            return None
+
+
+
         pass
 
     def set_fileHandler(self):
